f1e403d82224873bc53a2e472848918f49a17008
Wei <howard83124@gmail.com>
2018-03-30 12:03:34 +0800

    complete Day2

---
+++ src/components/Day2.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="day2">
+      <div class="center" @click="isActive = !isActive;" :class="{ active: isActive }">
+          <div class="line-1"></div>
+          <div class="line-2"></div>
+          <div class="line-3"></div>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Day2",
+  data() {
+    return {
+      isActive: false,
+      beenActive: false
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.day2 {
+  width: 400px;
+  height: 400px;
+  background-color: #3faf82;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3);
+  position: relative;
+}
+.center {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 80px;
+  height: 65px;
+  cursor: pointer;
+  //   .line {
+  //     z-index: 1;
+  //     position: absolute;
+  //     left: 50%;
+  //     top: 50%;
+  //     transform: translate(-50%, -50%);
+  //     width: 95%;
+  //     height: 8px;
+  //     background-color: #fff;
+  //     box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);
+  //     border-radius: 3px;
+  //     &::after,
+  //     &::before {
+  //       z-index: 10;
+  //       position: absolute;
+  //       left: 0px;
+  //       content: "";
+  //       width: 100%;
+  //       height: 8px;
+  //       background-color: #fff;
+  //       box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);
+  //       border-radius: 3px;
+  //     }
+  //     &::after {
+  //       top: 25px;
+  //     }
+  //     &::before {
+  //       top: -25px;
+  //     }
+  //   }
+  .line-1 {
+    width: 95%;
+    height: 8px;
+    background-color: #fff;
+    box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);
+    border-radius: 3px;
+    animation: line-1-rev 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+  }
+  .line-2 {
+    left: 0px;
+    width: 95%;
+    height: 8px;
+    background-color: #fff;
+    box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);
+    border-radius: 3px;
+    margin: 15px 0;
+    animation: line-2-rev 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+  }
+  .line-3 {
+    left: 0px;
+    width: 95%;
+    height: 8px;
+    background-color: #fff;
+    box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);
+    border-radius: 3px;
+    animation: line-3-rev 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+  }
+}
+.no-animation {
+  animation: none !important;
+}
+$cubic-bezier-in: cubic-bezier(0.3, 1, 0.7, 1);
+.active {
+  .line-1 {
+    animation: line-1 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+  }
+  .line-2 {
+    animation: line-2 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+  }
+  .line-3 {
+    animation: line-3 0.7s cubic-bezier(0.3, 1, 0.7, 1) forwards;
+  }
+}
+
+@keyframes line-1 {
+  0% {
+  }
+  50% {
+    transform: translateY(23px) rotate(0);
+  }
+  100% {
+    transform: translateY(23px) rotate(45deg);
+  }
+}
+@keyframes line-2 {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(0);
+    opacity: 0;
+  }
+}
+@keyframes line-3 {
+  0% {
+  }
+  50% {
+    transform: translateY(-23px) rotate(0);
+  }
+  100% {
+    transform: translateY(-23px) rotate(135deg);
+  }
+}
+
+@keyframes line-1-rev {
+  0% {
+    transform: translateY(23px) rotate(45deg);
+  }
+  50% {
+    transform: translateY(23px) rotate(0);
+  }
+  100% {
+  }
+}
+@keyframes line-2-rev {
+  0% {
+    transform: scale(0);
+    opacity: 0;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+@keyframes line-3-rev {
+  0% {
+    transform: translateY(-23px) rotate(135deg);
+  }
+  50% {
+    transform: translateY(-23px) rotate(0);
+  }
+  100% {
+  }
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Day1 from '@/components/Day1'
+import Day2 from '@/components/Day2'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
-    path: '/Day1',
-    name: 'Day1',
-    component: Day1
-  }]
+      path: '/Day1',
+      name: 'Day1',
+      component: Day1
+    },
+    {
+      path: '/Day2',
+      name: 'Day2',
+      component: Day2
+    }
+  ]
 })
