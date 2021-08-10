a4547077d262a26291ad9e9a7b09d66ffffadccb
Wei <howard83124@gmail.com>
2021-08-10 17:56:16 +0800

    adjust eslint sort rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,14 @@ module.exports = {
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
   ],
+  plugins: ['simple-import-sort', 'prettier'],
   parserOptions: {
     ecmaVersion: 2021,
   },
-  rules: {},
+  rules: {
+    'prettier/prettier': 'error',
+    'import/order': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 }
