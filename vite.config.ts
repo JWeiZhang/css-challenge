bcd6f94319cf95d56a238784e3f0e3497732b3c1
Wei <howard83124@gmail.com>
2021-09-06 21:54:02 +0800

    add host config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/css-challenge/' : '',
   server: {
     port: 8080,
+    host: '0.0.0.0',
   },
   resolve: {
     alias: {
